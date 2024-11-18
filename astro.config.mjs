bef7b86c36a158fb6afd81a1084d5c4b638964f6
Joe Cordingley <joewcordingley@gmail.com>
2024-11-18 20:47:16 +0000

    finished tutorial

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,10 +3,12 @@ import { defineConfig } from "astro/config";
 
 import cloudflare from "@astrojs/cloudflare";
 
+import preact from "@astrojs/preact";
+
 // https://astro.build/config
 export default defineConfig({
   site: "http://www.joecordingley.com",
   output: "server",
   adapter: cloudflare(),
+  integrations: [preact()],
 });
-
