d172b4bc0464e54a0dc42f57e122f340ffce6082
Joe Cordingley <joewcordingley@gmail.com>
2025-01-05 23:07:19 +0000

    revert 7cfed7757e4ec483d9bae1577abddb84c4db345d and 4361b64bab033d20cf28495e120a7e614e54f910

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -1,16 +1,11 @@
-import rss from '@astrojs/rss';
-import { getCollection } from 'astro:content';
-import { siteConfig } from '../consts';
+import rss, { pagesGlobToRssItems } from "@astrojs/rss";
 
 export async function GET(context) {
-	const posts = await getCollection('blog');
-	return rss({
-		title: siteConfig.title,
-		description: siteConfig.description,
-		site: context.site,
-		items: posts.map((post) => ({
-			...post.data,
-			link: `/blog/${post.slug}/`,
-		})),
-	});
+  return rss({
+    title: "Astro Learner | Blog",
+    description: "My journey learning Astro",
+    site: context.site,
+    items: await pagesGlobToRssItems(import.meta.glob("./**/*.md")),
+    customData: `<language>en-us</language>`,
+  });
 }
