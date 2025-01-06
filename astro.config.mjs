8faa1a0d2596e26018039dc7450186d704041a78
JoeCordingley <joe@txodds.com>
2025-01-06 18:04:37 +0000

    wip

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,10 +5,12 @@ import cloudflare from "@astrojs/cloudflare";
 
 import preact from "@astrojs/preact";
 
+import tailwind from "@astrojs/tailwind";
+
 // https://astro.build/config
 export default defineConfig({
   site: "http://www.joecordingley.com",
   output: "server",
   adapter: cloudflare(),
-  integrations: [preact()],
+  integrations: [preact(), tailwind()],
 });
